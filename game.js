f2241dad933a2b99f7bd532a1bec55dea24f8ab1
Loza <97474199+Lolah101@users.noreply.github.com>
2023-09-30 07:34:53 +0100

    used local storage to track game high score

--- game.js
+++ game.js
@@ -3,12 +3,15 @@ const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
 const countdownBoard = document.querySelector(".countdown");
 const startButton = document.querySelector(".startButton");
+const highScoreBoard = document.querySelector(".highScore");
 
 let lastHole;
 let timeUp = false;
 let timeLimit = 20000;
 let score = 0;
 let countdown;
+let highScore = localStorage.getItem("game1HighScore") || 0;
+highScoreBoard.textContent = "HIGH SCORE: " + highScore;
 
 function pickRandomHole(holes) {
   const randomHole = Math.floor(Math.random() * holes.length);
@@ -48,6 +51,7 @@ function startGame() {
     if (countdown < 0) {
       countdown = 0;
       clearInterval(startCountdown);
+      checkHighScore();
       countdownBoard.textContent =
         "Times Up! Thank you for protecting our planet! This is the way!";
     }
@@ -59,12 +63,20 @@ startButton.addEventListener("click", startGame);
 function whack(e) {
   score++;
   this.style.backgroundImage = 'url("background-vid/yoda2.png")';
-  this.style.pointerEvents = 'none';
+  this.style.pointerEvents = "none";
   setTimeout(() => {
     this.style.backgroundImage = 'url("background-vid/yoda1.png")';
-    this.style.pointerEvents = 'all';
+    this.style.pointerEvents = "all";
   }, 800);
   scoreBoard.textContent = score;
 }
 
 moles.forEach((mole) => mole.addEventListener("click", whack));
+
+function checkHighScore() {
+  if (score > localStorage.getItem("game1HighScore ")) {
+    localStorage.setItem("game1HighScore", score);
+    highScore = score;
+    highScoreBoard.textContent = "HIGH SCORE:" + highScore;
+  }
+}
